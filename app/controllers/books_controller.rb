9307c2d5dd8e48493fc3fabc26ee1de89bbb9927
Hieu Nguyen <hieuk09@gmail.com>
2015-06-18 11:33:40 +0700

    Use benchmark-ips instead

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,6 @@
 class BooksController < ApplicationController
   def index
-    books = Book.all.includes(:author)
+    books = Book.limit(50).includes(:author)
     render json: Oj.dump(
       ActiveModel::ArraySerializer.new(books, serializer: BookSerializer).as_json
     )
