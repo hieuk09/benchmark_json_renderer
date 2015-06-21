6c25450ad1c5c438b79666c5a91aac2ce28f1e7e
Hieu Nguyen <hieuk09@gmail.com>
2015-06-21 18:41:00 +0700

    [f] Add more benchmarks

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,24 @@
 class BooksController < ApplicationController
   def index
-    books = Book.limit(50).includes(:author)
-    render json: Oj.dump(books.as_api_response(:default))
+    books = Book.latest.includes(:author, { related_books: :author })
+    klass = set_klass
+    render json: Oj.dump(books.as_api_response(klass))
+  end
+
+  def show
+    book = Book.find(params[:id])
+    klass = set_klass
+    render json: Oj.dump(book.as_api_response(klass))
+  end
+
+  def set_klass
+    case params[:type]
+    when 'ultra simple'
+      :ultra_simple
+    when 'simple'
+      :simple
+    when 'complex'
+      :complex
+    end
   end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,7 @@
 class Book < ActiveRecord::Base
   belongs_to :author
+  has_many :related_books, foreign_key: 'genre', primary_key: 'genre', class_name: 'Book'
+  scope :latest, -> { limit(50) }
 
   acts_as_api
 
