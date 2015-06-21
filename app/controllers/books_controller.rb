20b0182d7dd3f1389069e1b8ec1de76bd9c205bd
Hieu Nguyen <hieuk09@gmail.com>
2015-06-21 18:41:00 +0700

    [f] Add more benchmarks

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,24 @@
 class BooksController < ApplicationController
   def index
-    books = Book.limit(50).includes(:author)
-    render json: BookRepresenter.for_collection.prepare(books)
+    books = Book.latest.includes(:author, { related_books: :author })
+    klass = set_klass
+    render json: klass.for_collection.prepare(books)
+  end
+
+  def show
+    book = Book.find(params[:id])
+    klass = set_klass
+    render json: klass.prepare(book)
+  end
+
+  def set_klass
+    case params[:type]
+    when 'ultra simple'
+      Books::UltraSimple
+    when 'simple'
+      Books::Simple
+    when 'complex'
+      Books::Complex
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  resources :books, only: [:index]
+  resources :books, only: [:show, :index]
 end
