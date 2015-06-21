2720d91eee8beb57fbdcb5886132607891e213f7
Hieu Nguyen <hieuk09@gmail.com>
2015-06-21 18:41:00 +0700

    [f] Add more benchmarks

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,24 @@
 class BooksController < ApplicationController
   def index
-    @books = Book.limit(50).includes(:author)
+    @books = Book.latest.includes(:author, { related_books: :author })
+    template = set_template
+    render "#{template}_collection"
+  end
+
+  def show
+    @book = Book.find(params[:id])
+    template = set_template
+    render template
+  end
+
+  def set_template
+    case params[:type]
+    when 'ultra simple'
+      'ultra_simple'
+    when 'simple'
+      'simple'
+    when 'complex'
+      'complex'
+    end
   end
 end
--- spec/api/books_spec.rb
+++ spec/api/books_spec.rb
@@ -2,11 +2,33 @@ require 'rails_helper'
 require 'benchmark/ips'
 
 describe 'books api' do
+  let(:ultra_simple_params) { { type: 'ultra simple' } }
+  let(:simple_params) { { type: 'simple' } }
+  let(:complex_params) { { type: 'complex' } }
+
   describe '/books' do
+    let!(:path) { "/books/" }
+
     it 'benchmark' do
       Benchmark.ips do |x|
-        x.config(:time => 10, :warmup => 2)
-        x.report { get '/books.json' }
+        x.config(time: 10, warmup: 2)
+        x.report('collection ultra simple') { get(path, ultra_simple_params) }
+        x.report('collection simple') { get path, simple_params }
+        x.report('collection complex') { get path, complex_params }
+      end
+    end
+  end
+
+  describe '/book/:id' do
+    let(:book) { Book.last }
+    let!(:path) { "/books/#{book.id}" }
+
+    it 'benchmarks' do
+      Benchmark.ips do |x|
+        x.config(time: 10, warmup: 2)
+        x.report('ultra simple') { get(path, ultra_simple_params) }
+        x.report('simple') { get path, simple_params }
+        x.report('complex') { get path, complex_params }
       end
     end
   end
