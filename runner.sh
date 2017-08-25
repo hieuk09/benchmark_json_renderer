f339f37cef5467b3c8f26a45e923430985afc3d4
Hieu Nguyen <hieuk09@gmail.com>
2017-08-25 14:16:50 +0700

    [f] Add runner

---
+++ runner.sh
@@ -0,0 +1,9 @@
+ #!/bin/bash
+
+ array=("master" "acts_as_api" "active_model_serializers" "grape_entity" "jbuilder" "jbuilder_with_partial" "rabl" "roar")
+ for i in "${array[@]}"
+ do
+   env -i git checkout $i
+   echo $i
+   rspec
+ done
